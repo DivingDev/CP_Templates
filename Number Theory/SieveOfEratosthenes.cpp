8f64a7679b99e1537ed0b0ce83ca4586c7b251b2
DivingDev <anandrudra2002@gmail.com>
2024-02-27 22:22:43 +0530

    edited

--- Number Theory/SieveOfEratosthenes.cpp
+++ Number Theory/SieveOfEratosthenes.cpp
@@ -4,12 +4,14 @@ using namespace std;
 // Time Complexity = O(n log log n)
 // Space Complexity = O(n)
 
-void SieveOfEratosthenes(int n) {
-    vector<bool> is_prime(n+1, true);
+// replacing n with 1e6 for generalization
+
+vector<bool> is_prime(1e6+1, true);
+void SieveOfEratosthenes() {
     is_prime[0] = is_prime[1] = false;
-    for (int i = 2; i * i <= n; i++) {
+    for (int i = 2; i * i <= 1e6; i++) {
         if (is_prime[i]) {
-            for (int j = i * i; j <= n; j += i)
+            for (int j = i * i; j <= 1e6; j += i)
                 is_prime[j] = false;
         }
     }
