239a4c4b643ca31f34558bc0165a96553b112c45
DivingDev <anandrudra2002@gmail.com>
2024-04-15 12:04:15 +0530

    more efficient code

--- Graph/Dijkstra.cpp
+++ Graph/Dijkstra.cpp
@@ -1,24 +1,32 @@
-// O((N+E)*log(N))
+#include <bits/stdc++.h>
+using namespace std;
+#define pii pair<int, int>
+// O(n + m*logm)  where n is no. of nodes and m is no. of edges
 // Single source sohortest path
 void dijkstra(int n, int s, vector<pii> adj[])
 {
-    priority_queue<pii, vector<pii>, greater<pii>> pq;
-    pq.push({0, s});
-    vector<int> dist(n, INF);
+    priority_queue<pii> pq;   //{dist,node}
+    vector<int> dist(n + 1, 1e9);
+    vector<bool> vis(n + 1, 0);
     dist[s] = 0;
+    pq.push({0, s});
     while (!pq.empty())
     {
-        int node = pq.top().second;
-        int dis = pq.top().first;
+        int u = pq.top().second;
         pq.pop();
-        for (auto it : adj[node])
+
+        if (vis[u])
+            continue;
+        vis[u] = 1;
+
+        for (auto it : adj[u])
         {
-            int adjNode = it.first;
             int edW = it.second;
-            if (dist[adjNode] > dist[node] + edW)
+            int v = it.first;
+            if (dist[v] > dist[u] + edW)
             {
-                dist[adjNode] = dist[node] + edW;
-                pq.push({dist[adjNode], adjNode});
+                dist[v] = dist[u] + edW;
+                pq.push({-dist[v], v});
             }
         }
     }
