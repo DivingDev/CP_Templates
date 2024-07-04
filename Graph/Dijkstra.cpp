ac3f3bdc8801ec529d054ea806cda8b602d46ee9
DivingDev <anandrudra2002@gmail.com>
2024-07-04 09:38:10 +0530

    print

--- Graph/Dijkstra.cpp
+++ Graph/Dijkstra.cpp
@@ -5,11 +5,12 @@ using namespace std;
 // Single source sohortest path
 void dijkstra(int n, int s, vector<pii> adj[])
 {
-    priority_queue<pii> pq;   //{dist,node}
+    priority_queue<pii> pq; //{dist,node}
     vector<int> dist(n + 1, 1e9);
     vector<bool> vis(n + 1, 0);
     dist[s] = 0;
     pq.push({0, s});
+    vector<int> parent(n + 1, -1); //  only if want to print path
     while (!pq.empty())
     {
         int u = pq.top().second;
@@ -27,7 +28,23 @@ void dijkstra(int n, int s, vector<pii> adj[])
             {
                 dist[v] = dist[u] + edW;
                 pq.push({-dist[v], v});
+                // updating for path
+                parent[v] = u;
             }
         }
     }
+}
+
+// function takes starting vertex s
+// path to any vertex t can be restored in following way
+vector<int> restore_path(int s, int t, vector<int> const &p)
+{
+    vector<int> path;
+
+    for (int v = t; v != s; v = p[v])
+        path.push_back(v);
+    path.push_back(s);
+
+    reverse(path.begin(), path.end());
+    return path;
 }
